2be5272ca22fcd80b8cd226971c0a7a0c48b9dfc
AnnaToimurzina <2402as@gmail,com>
2023-05-21 22:49:07 +0500

    add commit

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,9 +1,9 @@
 from src.channel import Channel
 
 if __name__ == '__main__':
-    moscowpython = Channel('AIzaSyDalbhEObqKUPZFaIcUu3DMDk5r3alExc0')
+    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
     moscowpython.print_info()
-
+# AIzaSyDalbhEObqKUPZFaIcUu3DMDk5r3alExc0
     """
 {
   "kind": "youtube#channelListResponse",
--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,12 @@
 import json
 import os
-
 # необходимо установить через: pip install google-api-python-client
 from googleapiclient.discovery import build
 
-import isodate
+# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+api_key: str = os.getenv('YT_API_KEY', 'AIzaSyDalbhEObqKUPZFaIcUu3DMDk5r3alExc0')
 
-from helper.youtube_api_manual import printj
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
@@ -16,71 +16,9 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
-
     def print_info(self) -> None:
-        # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-        api_key: str = os.getenv('YT_API_KEY')
-
-        # создать специальный объект для работы с API
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        """Выводит словарь в json-подобном удобном формате с отступами"""
-        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
-
-        ''' получить данные о канале по его id docs: https://developers.google.com/youtube/v3/docs/channels/list 
-        сервис для быстрого получения id канала: https://commentpicker.com/youtube-channel-id.php'''
-        # channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
-
-        channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
-        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-        printj(channel)
-
-
-        '''получить данные по play-листам канала
-docs: https://developers.google.com/youtube/v3/docs/playlists/list'''
-
-        playlists = youtube.playlists().list(channelId=channel_id,
-                                             part='contentDetails,snippet',
-                                             maxResults=50,
-                                             ).execute()
-        for playlist in playlists['items']:
-            print(playlist)
-            print()
-
-        '''получить данные по видеороликам в плейлисте
-docs: https://developers.google.com/youtube/v3/docs/playlistItems/list
-
-получить id плейлиста можно из браузера, например
-https://www.youtube.com/playlist?list=PLH-XmS0lSi_zdhYvcwUfv0N88LQRt6UZn
-или из ответа API: см. playlists выше'''
-
-        playlist_id = 'PLH-XmS0lSi_zdhYvcwUfv0N88LQRt6UZn'
-        playlist_videos = youtube.playlistItems().list(playlistId=playlist_id,
-                                                       part='contentDetails',
-                                                       maxResults=50,
-                                                       ).execute()
-        # printj(playlist_videos)
-
-        # получить все id видеороликов из плейлиста
-        video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
-        # print(video_ids)
-
-        '''
-        вывести длительности видеороликов из плейлиста
-        docs: https://developers.google.com/youtube/v3/docs/videos/list
-        '''
-        video_response = youtube.videos().list(part='contentDetails,statistics',
-                                               id=','.join(video_ids)
-                                               ).execute()
-        # printj(video_response)
-
-        for video in video_response['items']:
-            # YouTube video duration is in ISO 8601 format
-            iso_8601_duration = video['contentDetails']['duration']
-            duration = isodate.parse_duration(iso_8601_duration)
-            print(duration)
-
-
-
-
-
+        """Выводит в консоль информацию о канале."""
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        info = json.dumps(channel, indent=2, ensure_ascii=False)
+        print(info)
 
