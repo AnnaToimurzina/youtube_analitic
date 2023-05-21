aee827333fd35116b13aef52d935432dcaea35a9
AnnaToimurzina <2402as@gmail,com>
2023-05-21 08:58:04 +0500

    add commit

--- src/channel.py
+++ src/channel.py
@@ -2,7 +2,7 @@ import json
 import os
 
 # необходимо установить через: pip install google-api-python-client
-from google-api-client.discovery import build
+from googleapiclient.discovery import build
 
 import isodate
 
@@ -16,13 +16,13 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
+
+    def print_info(self) -> None:
         # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
         api_key: str = os.getenv('YT_API_KEY')
 
         # создать специальный объект для работы с API
         youtube = build('youtube', 'v3', developerKey=api_key)
-
-    def print_info(self) -> None:
         """Выводит словарь в json-подобном удобном формате с отступами"""
         print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
@@ -45,6 +45,55 @@ docs: https://developers.google.com/youtube/v3/docs/playlists/list'''
         for playlist in playlists['items']:
             print(playlist)
             print()
+
+        '''получить данные по видеороликам в плейлисте
+docs: https://developers.google.com/youtube/v3/docs/playlistItems/list
+
+получить id плейлиста можно из браузера, например
+https://www.youtube.com/playlist?list=PLH-XmS0lSi_zdhYvcwUfv0N88LQRt6UZn
+или из ответа API: см. playlists выше'''
+
+        playlist_id = 'PLH-XmS0lSi_zdhYvcwUfv0N88LQRt6UZn'
+        playlist_videos = youtube.playlistItems().list(playlistId=playlist_id,
+                                                       part='contentDetails',
+                                                       maxResults=50,
+                                                       ).execute()
+        # printj(playlist_videos)
+
+        # получить все id видеороликов из плейлиста
+        video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
+        # print(video_ids)
+
+        '''
+        вывести длительности видеороликов из плейлиста
+        docs: https://developers.google.com/youtube/v3/docs/videos/list
+        '''
+        video_response = youtube.videos().list(part='contentDetails,statistics',
+                                               id=','.join(video_ids)
+                                               ).execute()
+        # printj(video_response)
+
+        for video in video_response['items']:
+            # YouTube video duration is in ISO 8601 format
+            iso_8601_duration = video['contentDetails']['duration']
+            duration = isodate.parse_duration(iso_8601_duration)
+            print(duration)
+
+        '''
+        получить статистику видео по его id
+        получить id можно из адреса видео
+        https://www.youtube.com/watch?v=gaoc9MPZ4bw или https://youtu.be/gaoc9MPZ4bw
+        '''
+        video_id = 'gaoc9MPZ4bw'
+        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=video_id
+                                               ).execute()
+        # printj(video_response)
+        video_title: str = video_response['items'][0]['snippet']['title']
+        view_count: int = video_response['items'][0]['statistics']['viewCount']
+        like_count: int = video_response['items'][0]['statistics']['likeCount']
+        comment_count: int = video_response['items'][0]['statistics']['commentCount']
+
 
 
 
